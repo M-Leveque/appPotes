96e796abd8315b8707c3344ed162ab65f89252c3
CorantinLV <corantin.levagueresse@gmail.com>
2018-09-08 22:08:45 +0200

    Installation font et normalize.css

--- ressources/vues/VueNav.php
+++ ressources/vues/VueNav.php
@@ -4,6 +4,9 @@
 <meta name="viewport" 				content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" 	content="IE=EmulateIE9">
 <title>APP POTES</title>
+<!-- normalize.css -->
+<link href="../css/normalize.css" rel="stylesheet" type="text/css">
+<!-- fonts -->
 </head>
 
 <body>
