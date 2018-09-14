d13dc43eeb6ec121fda6607cbd895fcee1fd0c25
CorantinLV <corantin.levagueresse@gmail.com>
2018-09-14 18:01:03 +0200

    ok

--- ressources/vues/VueConnexion.php
+++ ressources/vues/VueConnexion.php
@@ -64,12 +64,4 @@
 		</section>
   		<a href="#" rel="modal:close">Close</a>
 	</div><!-- #Modal-Content -->
-	
 </main>
-
-<!-------------------------------------
-	SCRIPT
---------------------------------------->
-<!-- Modal -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
