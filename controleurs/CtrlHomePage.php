65ef28cfb4e5753fb7c05d1931c8d530a13fac60
ordinateur <leveque.melvin@gmail.com>
2018-09-23 00:58:16 +0200

    test toogle

--- controleurs/CtrlHomePage.php
+++ controleurs/CtrlHomePage.php
@@ -2,3 +2,4 @@
 include_once('ressources/vues/VueHeader.php');
 include_once('ressources/vues/VueNav.php');
 include_once('ressources/vues/VueHomePage.php');
+include_once('ressources/vues/VueFooter.php');
