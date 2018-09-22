21a91397ac3825470ab65bc13a853eeb481ef840
ordinateur <leveque.melvin@gmail.com>
2018-09-23 00:53:15 +0200

    changement chemin absolut

--- ressources/vues/VueFooter.php
+++ ressources/vues/VueFooter.php
@@ -6,5 +6,5 @@
 <!-- TOGGLE -->
 <script src="//code.jquery.com/jquery-1.12.4.js"></script>
 <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="/ressources/js/toggle.js"></script>
+<script src="ressources/js/toggle.js"></script>
 </html>
--- ressources/vues/VueHomePage.php
+++ ressources/vues/VueHomePage.php
@@ -7,49 +7,49 @@
 			<h2>Derniers<br>Albums</h2>
 			<span></span>
 		</div>
-			
+
 		<div class="content-album"><!-- content -->
 			<div class="album album-com">
 			</div>
-			 
+
 			<!-- grid albums------------->
 			<div class="album album-grid">
-				<img src="/ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
+				<img src="ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
 				<span>Text</span>
 			</div>
-			
+
 			<div class="album album-grid">
-				<img src="/ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
+				<img src="ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
 				<span>Text</span>
 			</div>
-			
+
 			<div class="album album-grid">
-				<img src="/ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
+				<img src="ressources/img/vignettes/pic-2.jpg" alt="vignette de l'album">
 				<span>Text</span>
 			</div>
 			<!-- #grid albums ------------->
 		</div>
 	</section>
-	
+
 	<section><!-- EVENEMENTS -->
 		<div class="title-section"><!-- title -->
 			<h2>Événements</h2>
 			<span></span>
 		</div>
-		
+
 		<div><!-- content -->
-		
+
 		</div>
 	</section>
-	
+
 	<section><!-- CAGNOTTES -->
 		<div class="title-section"><!-- title -->
 			<h2>Cagnottes</h2>
 			<span></span>
 		</div>
-		
+
 		<div><!-- content -->
-		
+
 		</div>
 	</section>
 </main>
