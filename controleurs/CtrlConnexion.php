545c37160d535dc4927afa25f6d26db6771b2d40
ordinateur <leveque.melvin@gmail.com>
2018-09-23 00:49:02 +0200

    test bug safari

--- controleurs/CtrlConnexion.php
+++ controleurs/CtrlConnexion.php
@@ -3,7 +3,24 @@
 //---------------------| Traitement php de la page Connexion |-------------------------
 //-------------------------------------------------------------------------------------
 
+//On inclut la class DAO (Data Access Objet)
+include('modeles/src/DAO/UtilisateurDAO.class.php');
 
+//On se connecte � la base de donn�es
+$utilisateurDao = new UtilisateurDAO();
+
+// on verifie les parametres de connexion
+$ident = !empty($_POST['ident']) ? $_POST['ident'] : NULL;
+$mdp = !empty($_POST['mdp']) ? $_POST['mdp'] : NULL;
+
+//Si on recoit en post les donn�es de connexion utilisateur alors essaye de se logger
+if(isset($ident) AND isset($mdp)){
+
+   if($utilisateurDao->login($ident , $mdp ) == true){
+        //Si la connexion reussi, on envoie vers la HomePage
+      header("Location: index.php?action=HomePage");
+    }
+}
 
 //-------------------------------------------------------------------------------------
 //-------------------------------| Insertion des vues |--------------------------------
