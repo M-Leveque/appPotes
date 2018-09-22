525ac63b3ecf08b34821d6c2f1561dff3d145050
CorantinLV <corantin.levagueresse@gmail.com>
2018-09-23 01:06:43 +0200

    toggle nav

--- ressources/vues/VueNav.php
+++ ressources/vues/VueNav.php
@@ -23,8 +23,3 @@
 		<a href="#"><div class="icon icon-add-cash"></div><span>Ajouter<br>une cagnotte</span></a>
 	</ul>
 </section>
-
-
-<script src="//code.jquery.com/jquery-1.12.4.js"></script>
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="ressources/js/toggle.js"></script>
