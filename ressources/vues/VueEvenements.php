976bb0935da4be0f473352c03a2494b6c0681a50
CorantinLV <corantin.levagueresse@gmail.com>
2018-10-10 19:46:50 +0200

    Début style page evenement

---
+++ ressources/vues/VueEvenements.php
@@ -0,0 +1,79 @@
+<main id="content-even">
+	<div class="title">
+		<h2>Événements</h2>
+	</div>
+	
+	<section class="grid-gen grid-even">
+		<a class="even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+				
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+				
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+		
+		<a class="even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+				
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+				
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+	
+		<a class="even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+		
+		<!-- AFFICHAGE ANCIEN EVENEMENT -->
+		<button><i><u>Afficher les événements passés</u></i></button>
+		
+		<a class="even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+		
+	</section>
+</main>
--- ressources/vues/VueHeader.php
+++ ressources/vues/VueHeader.php
@@ -1,4 +1,5 @@
-<html lang="fr"><head>
+<html lang="fr">
+<head>
 <meta charset="UTF-8">
 <meta name="viewport" 				content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" 	content="IE=EmulateIE9">
