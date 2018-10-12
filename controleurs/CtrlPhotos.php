a4b051f773a25ea4f7ddd632009dd40b642d09ff
CorantinLV <corantin.levagueresse@gmail.com>
2018-10-12 19:22:39 +0200

    Modification "CtrlPhotos.php" par Melvin

--- controleurs/CtrlPhotos.php
+++ controleurs/CtrlPhotos.php
@@ -1,19 +1,18 @@
 <?php
-include_once "modeles/DAO/AlbumDAO.class.php";
-include_once "modeles/Album.class.php";
+include_once("modeles/DAO/AlbumDAO.class.php");
+include_once("modeles/Album.class.php");
 
 include_once('ressources/vues/VueHeader.php');
 include_once('ressources/vues/VueNav.php');
 
 //Affichage de l'album selon la requete
 if(isset($_GET["Album"])){
-  //On recupere l'album en question dans la bdd
-  $albumDAO = new albumDAO();
-  $album = $albumDAO->get(intval($_GET["Album"]));
-  $dateCreation= "00/00/0000"; //A implementer
-  $titreAlbum = $album->getNom();
+	//On recupere l'album en question dans la bdd
+   //$albumDAO = new albumDAO();
+   //$album = $albumDAO->get(intval($_GET["Album"]));
+//  $dateCreation= "00/00/0000"; //A implementer
+//  $titreAlbum = $album->getNom();
 }
-
 
 include_once('ressources/vues/VuePhotos.php');
 include_once('ressources/vues/VueFooter.php');
