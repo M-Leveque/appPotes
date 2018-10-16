eba2286a08dd72cb30e93750ce4f289d392721ae
ordinateur <leveque.melvin@gmail.com>
2018-10-16 14:45:48 +0200

    Mise à jour AlbumTestDAO

--- modeles/tests/AlbumDAOTest.php
+++ modeles/tests/AlbumDAOTest.php
@@ -24,7 +24,7 @@ class AlbumDAOTest extends TestCase
     $utilisateur = new Utilisateur(1, 0, 'Visiteur@visiteur.com', 'visiteur1', 'Visiteur', 'img.png', false);
     $emoticon = new Emoticon(1, "content", "/images/img.png");
     $evenement = new Evenement(1, "Vacance 2011", "Ete au camping", "2018-01-03", "2018-05-02 22:00:00", false, $utilisateur, $emoticon);
-    $album = new Album(1 ,"Vacance 2011", false ,"ressources/images/visuel/visuel1.png", $evenement, $utilisateur);
+    $album = new Album(1 ,"Vacance 2011", "Photos des vacances", "2015-05-02",false ,"ressources/images/visuel/visuel1.png", $evenement, $utilisateur);
 
     $this->assertEquals($albumDAO->get(1)->getId(), $album->getId());
   }
