da17c834c8f65ff27cdb376a0a97b2da620be6cc
CorantinLV <corantin.levagueresse@gmail.com>
2018-10-17 19:32:57 +0200

    ok

--- ressources/vues/VueEvenements.php
+++ ressources/vues/VueEvenements.php
@@ -55,10 +55,44 @@
 			</div>
 		</a>
 		
-		<!-- AFFICHAGE ANCIEN EVENEMENT -->
+		<!-- BUTTON AFFICHAGE ANCIEN EVENEMENT -->
 		<button class="even"><i><u>Afficher les événements passés</u></i></button>
 		
-		<a class="even" href="#">
+		<a class="even old-even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+		
+		<a class="even old-even" href="#">
+			<div class="content-even">
+				<span>ðŸŽ</span>
+
+				<div class="desc-even">
+					<h6>Anniversaire Bastien</h6>
+					<p>DÃ©scription Ã©vÃ©nement Sp cum ducibus est civitas memoriam crudelitatem.</p>
+				</div>
+
+				<div class="date-even">
+					<p>VEN</p>
+					<span>14</span>
+					<p>JUIN</p>
+				</div>
+			</div>
+		</a>
+		
+		<a class="even old-even" href="#">
 			<div class="content-even">
 				<span>ðŸŽ</span>
 
