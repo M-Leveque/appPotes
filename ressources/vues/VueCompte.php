e163f9c5de38685ecb01f42e8e24c8bec6f3befe
CorantinLV <corantin.levagueresse@gmail.com>
2018-10-27 19:08:25 +0200

    Début intégration page compte

---
+++ ressources/vues/VueCompte.php
@@ -0,0 +1,14 @@
+<main id="content-compte">
+	
+	<div class="profil-pic">
+		<img src="ressources/img/profile/pic-3.jpg" alt="photo de profile du compte">
+	</div>
+	
+	<div class="title">
+		<h2>Hey !<br>Corantin 😀</h2>
+	</div>
+	
+	<div class="profil-opt">
+		<a class="icon icon-down" href="#"></a>
+		<a class="icon icon-sitting" href="#"></a>
+	</div>
