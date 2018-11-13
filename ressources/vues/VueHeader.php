c7d69e2e9c83019865dd3dc37ff9b2cfa6fec57c
CorantinLV <corantin.levagueresse@gmail.com>
2018-11-13 20:36:14 +0100

    Modif lien css

--- ressources/vues/VueHeader.php
+++ ressources/vues/VueHeader.php
@@ -5,18 +5,18 @@
 <meta http-equiv="X-UA-Compatible" 	content="IE=EmulateIE9">
 <title>APP POTES</title>
 	<!-- Fonts -->
-<link href="/ressources/css/font.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/font.css" rel="stylesheet" type="text/css">
 	<!-- Normalize.css -->
-<link href="/ressources/css/normalize.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/normalize.css" rel="stylesheet" type="text/css">
 	<!-- Style css -->
-<link href="/ressources/css/Style.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/Style.css" rel="stylesheet" type="text/css">
 	<!-- Style Nav -->
-<link href="/ressources/css/StyleNav.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/StyleNav.css" rel="stylesheet" type="text/css">
 	<!-- Style Connexion -->
-<link href="/ressources/css/StyleConnexion.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/StyleConnexion.css" rel="stylesheet" type="text/css">
 	<!-- Style Galerie Photos -->
-<link href="/ressources/js/gallery/lightgallery.min.css" rel="stylesheet" type="text/css">
+<link href="ressources/js/gallery/lightgallery.min.css" rel="stylesheet" type="text/css">
     <!-- Style Photos -->
-<link href="/ressources/css/StylePhotos.css" rel="stylesheet" type="text/css">
+<link href="ressources/css/StylePhotos.css" rel="stylesheet" type="text/css">
 </head>
 <body>
