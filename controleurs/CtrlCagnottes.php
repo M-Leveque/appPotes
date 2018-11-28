658bdd5b35a94b62bdede36cb218845ea66b67ad
M-Leveque <leveque.melvin@gmail.com>
2018-11-28 21:40:38 +0100

    Cagnotte : fin affichage dynamique

--- controleurs/CtrlCagnottes.php
+++ controleurs/CtrlCagnottes.php
@@ -1,4 +1,20 @@
 <?php
+include_once('modeles/DAO/CagnotteDAO.class.php');
+
+//Cagnottes
+try{
+    $cagnotteDAO = new CagnotteDAO();
+    $cagnottesInfos = $cagnotteDAO->getAll();
+    $cagnottes = "";
+
+    foreach ($cagnottesInfos as $cagnotteInfo){
+        $cagnottes .= Cagnotte::toHTML($cagnotteInfo->getId(), $cagnotteInfo->getTitre(), $cagnotteInfo->getDescription(), $cagnotteInfo->getArgentR(), $cagnotteInfo->getDateHeureFin());
+    }
+}
+catch (Exception $e){
+    echo $e->getMessage();
+}
+
 include_once('ressources/vues/VueHeader.php');
 include_once('ressources/vues/VueNav.php');
 include_once('ressources/vues/VueCagnottes.php');
--- ressources/vues/VueCagnottes.php
+++ ressources/vues/VueCagnottes.php
@@ -5,55 +5,6 @@
 	
 	<!-- GRID CAGNOTTES -->
 	<section class="grid-gen grid-cagn">
-	<div class="content-cagn">
-			<div class="desc-cagn">
-				<h5>Cadeau Bastien</h5>
-				<p>DÃ©scription cagnottes Et eodem impetu Domitianum praecipitem per scalas itidem funibus constrinxerunt, eosque coniunctos per ampla spatia civitatis acri</p>
-				<button>Participer</button>
-			</div>
-
-			<div class="prix-cagn">
-				<span>51,00 â‚¬</span>
-				<p>Fin le<span> 00/00/00</span></p>
-			</div>
-		</div>
-		
-		<div class="content-cagn">
-			<div class="desc-cagn">
-				<h5>Cadeau Bastien</h5>
-				<p>DÃ©scription cagnottes Et eodem impetu Domitianum praecipitem per scalas itidem funibus constrinxerunt, eosque coniunctos per ampla spatia civitatis acri</p>
-				<button>Participer</button>
-			</div>
-
-			<div class="prix-cagn">
-				<span>51,00 â‚¬</span>
-				<p>Fin le<span> 00/00/00</span></p>
-			</div>
-		</div>
-		
-		<div class="content-cagn">
-			<div class="desc-cagn">
-				<h5>Cadeau Bastien</h5>
-				<p>DÃ©scription cagnottes Et eodem impetu Domitianum praecipitem per scalas itidem funibus constrinxerunt, eosque coniunctos per ampla spatia civitatis acri</p>
-				<button>Participer</button>
-			</div>
-
-			<div class="prix-cagn">
-				<span>51,00 â‚¬</span>
-				<p>Fin le<span> 00/00/00</span></p>
-			</div>
-		</div>
-		
-		<div class="content-cagn">
-			<div class="desc-cagn">
-				<h5>Cadeau Bastien</h5>
-				<p>DÃ©scription cagnottes Et eodem impetu Domitianum praecipitem per scalas itidem funibus constrinxerunt, eosque coniunctos per ampla spatia civitatis acri</p>
-				<button>Participer</button>
-			</div>
-
-			<div class="prix-cagn">
-				<span>51,00 â‚¬</span>
-				<p>Fin le<span> 00/00/00</span></p>
-			</div>
-		</div>
+        <?php echo $cagnottes ?>
+    </section>
 </main>
