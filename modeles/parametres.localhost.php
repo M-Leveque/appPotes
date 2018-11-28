74a74369425abd2e32498b67a05ab4dfa882e0dd
M-Leveque <leveque.melvin@gmail.com>
2018-11-28 21:23:21 +0100

    Evenement : Fin affichage dynamique page evenement

--- modeles/parametres.localhost.php
+++ modeles/parametres.localhost.php
@@ -9,5 +9,5 @@ global $PARAM_HOTE, $PARAM_PORT, $PARAM_BDD, $PARAM_USER, $PARAM_PWD;
 $PARAM_HOTE = "localhost";		// si le sgbd est sur la même machine que le serveur php
 $PARAM_PORT = "3306";			// le port utilisé par le serveur MySql
 $PARAM_BDD = "apppotes";	// nom de la base de données
-$PARAM_USER = "user";			// nom de l'utilisateur
-$PARAM_PWD = "user";		// son mot de passe
+$PARAM_USER = "root";			// nom de l'utilisateur
+$PARAM_PWD = "root";		// son mot de passe
